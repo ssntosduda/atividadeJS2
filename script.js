b03a9d5246096ffa8df39dfe2b299c2b868d270b
ssntosduda <144685408+ssntosduda@users.noreply.github.com>
2024-02-16 20:35:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let total = 10 + 5 * 2 / 2 + 20;
+console.log (total)
+
+let idade = '18 anos';
+let valor = '100BRL';
+console.log(isNaN(idade))
+console.log(isNaN(valor))
+
+let n1 = '200';
+let n2 = 50;
+let soma = +n1 + n2;
+console.log(soma)
+
+let incremento = 5;
+console.log(incremento++)
+console.log(incremento)
+
+let numero = '80';
+let unidade = 'kg';
+let peso = numero + unidade;
+console.log(peso)
+let pesoPorDois = (numero) / 2 + unidade;
+console.log(pesoPorDois)
